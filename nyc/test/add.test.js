f923a9808499b1b6b27e4c370f057538cc12d152
derycktse <285205451@qq.com>
2017-11-06 11:18:08 +0800

    nyc

--- nyc/test/add.test.js
+++ nyc/test/add.test.js
@@ -1,8 +1,20 @@
 var add = require('../add.js');
 var expect = require('chai').expect;
 
-describe('加法函数的测试', function() {
-  it('1 加 1 应该等于 2', function() {
+describe('加法函数的测试', function () {
+  beforeEach(function(done){
+    console.log('invode each time')
+    setTimeout(function () {
+      done('e2')
+    }, 1000)
+  })
+  it('1 加 1 应该等于 2', function () {
     expect(add(1, 1)).to.be.equal(2);
   });
+
+  it('test async', function (done) {
+    setTimeout(function () {
+      done(null)
+    }, 1000)
+  })
 });
