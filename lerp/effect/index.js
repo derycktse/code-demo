eb80e9ef3df53174a63c043bf68ed9c14b7a4c2b
Derycktse <285205451@qq.com>
2018-05-03 00:40:52 +0800

    修改

--- lerp/effect/index.js
+++ lerp/effect/index.js
@@ -4,35 +4,36 @@ var totalImages = 200;
 
 for (var i = 1; i < totalImages; i++) {
     var img = new Image;
-   
+
     var slug = '000' + i;
-    
-     img.src = 'https://s3.amazonaws.com/clearmotion/hero/high-min/frame'+ slug.slice(-3) +'-low.jpg'
+
+    img.src = 'https://s3.amazonaws.com/clearmotion/hero/high-min/frame' + slug.slice(-3) + '-low.jpg'
     images.push(img);
 }
 
 var c = document.getElementById("background");
 var ctx = c.getContext("2d");
 
-var mouseWheel = function () {
-    window.addEventListener('mousewheel', function (e) {
-        e.preventDefault(); // No scroll
+// var mouseWheel = function () {
+//     window.addEventListener('mousewheel', function (e) {
+//         e.preventDefault(); // No scroll
 
-        // The following equation will return either a 1 for scroll down
-        // or -1 for a scroll up
-        var delta = Math.max(-1, Math.min(1, e.wheelDelta));
+//         // The following equation will return either a 1 for scroll down
+//         // or -1 for a scroll up
+//         var delta = Math.max(-1, Math.min(1, e.wheelDelta));
 
-        // This code mostly keeps us from going too far in either direction
-        if (delta == -1) currentLocation += 1;
-        if (delta == 1) currentLocation -= 1;
-        if (currentLocation < 0) currentLocation = 0;
-        if (currentLocation >= (totalImages - 1)) currentLocation = (totalImages - 1);
-        console.log("Current location " + currentLocation);
+//         // This code mostly keeps us from going too far in either direction
+//         if (delta == -1) currentLocation += 1;
+//         if (delta == 1) currentLocation -= 1;
+//         if (currentLocation < 0) currentLocation = 0;
+//         if (currentLocation >= (totalImages - 1)) currentLocation = (totalImages - 1);
+//         console.log("Current location " + currentLocation);
 
-        // See below for the details of this function
-        setImage(currentLocation);
-    });
-}
+//         // See below for the details of this function
+//         setImage(currentLocation);
+//     });
+// }
+
 
 var setImage = function (newLocation) {
     // drawImage takes 5 arguments: image, x, y, width, height
@@ -43,5 +44,24 @@ var setImage = function (newLocation) {
 images[0].onload = function () {
     ctx.fillRect(0, 0, c.width, c.height);
     ctx.drawImage(images[currentLocation], 0, 0, 1000, 1000);
-    mouseWheel();
+    // mouseWheel();
 };
+
+
+let sy = 0;
+let dy = 0;
+render()
+function render() {
+    var sy = window.pageYOffset;
+    dy = lerp(dy, sy, 0.02);
+
+    dy = Math.floor(dy)
+
+    setImage(dy)
+
+    requestAnimationFrame(render)
+}
+
+function lerp(a, b, n) {
+    return (1 - n) * a + n * b;
+}
