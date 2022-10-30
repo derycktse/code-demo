b0f0b499d18ee17dba36556b439e40d14e27e8a8
deryck.xie <deryck.xie@deryckxiedeMacBook-Pro.local>
2022-10-30 15:49:55 +0800

    feat: 复现白屏死循坏

--- nextjs-union/next-handle/pages/_app.js
+++ nextjs-union/next-handle/pages/_app.js
@@ -4,16 +4,16 @@ import withRedux from 'next-redux-wrapper';
 import { createStore, compose, applyMiddleware } from 'redux';
 
 
-const reducer = (state = {tick: 'init', tack: 'init', toe: 'init'}, action) => {
+const reducer = (state = { tick: 'init', tack: 'init', toe: 'init' }, action) => {
   switch (action.type) {
-      case 'TICK':
-          return {...state, tick: action.payload};
-      case 'TACK':
-          return {...state, tack: action.payload};
-      case 'TOE':
-          return {...state, toe: action.payload};
-      default:
-          return state;
+    case 'TICK':
+      return { ...state, tick: action.payload };
+    case 'TACK':
+      return { ...state, tack: action.payload };
+    case 'TOE':
+      return { ...state, toe: action.payload };
+    default:
+      return state;
   }
 };
 
@@ -23,12 +23,16 @@ export const makeStore = initialState => {
   return store;
 };
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, optimize }) {
+
+  debugger
+  console.log('render optimize', optimize)
+  const { buildId } = optimize
   return (
     <div id="myapp">
       <Component {...pageProps} />
     </div>
-    )
+  )
 }
 
 // Only uncomment this method if you have blocking data requirements for
@@ -43,19 +47,24 @@ function MyApp({ Component, pageProps }) {
 //   return { ...appProps }
 // }
 
-MyApp.getInitialProps = async function({ Component, ctx }) {
+MyApp.getInitialProps = async function ({ Component, ctx }) {
 
+  console.log('_app getInitial Prop fired')
+  let optimize;
   if (typeof window === 'undefined') {
     // matchStateToCtx();
+    optimize = {}
   }
 
 
+  console.log('optimize', optimize)
   return {
     pageProps: {
       ...(Component.getInitialProps
         ? await Component.getInitialProps(ctx)
         : {}),
     },
+    optimize,
     // env: ctx.state.env || {},
     // region: ctx.state.country,
     // language: ctx.state.language,
